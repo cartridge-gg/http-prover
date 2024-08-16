f30c19cf398a02568da522901ae51384a5ddbbab
Mateusz Chudkowski <mateuszchudy03@gmail.com>
2024-08-16 13:23:29 +0200

    Port in tests as env, container_engine variable

--- prover-sdk/tests/sdk_test.rs
+++ prover-sdk/tests/sdk_test.rs
@@ -7,6 +7,7 @@ use prover_sdk::{access_key, errors, load};
 
 #[cfg(test)]
 mod tests {
+    use std::env;
     use std::path::PathBuf;
 
     use crate::load::{load_cairo0, load_cairo1};
@@ -17,43 +18,59 @@ mod tests {
     fn get_signing_key() -> ProverAccessKey {
         ProverAccessKey::from_hex_string(
             "0x5883b0e30b008e48af3d0bf5cfc138fb6093496da6f87d24b65def88470356d3",
-            // Corresponding to 0xd16b71c90dbf897e5964d2f267d04664b3c035036559d712994739ea6cf2fd9f public key.
         )
         .unwrap()
     }
     #[tokio::test]
     async fn test_prover_cairo0() -> Result<(), ProverSdkErrors> {
-        let prover_url = Url::parse("http://localhost:3040").unwrap(); // Provide an invalid URL
+        let port = env::var("PORT").unwrap();
+        let prover_url = Url::parse(&format!("http://localhost:{}", port)).unwrap();
+
         let sdk = ProverSDK::new(get_signing_key(), prover_url).await?;
 
         let data = load_cairo0(PathBuf::from("../examples/CairoZero/prover_input.json")).await?;
         let proof = sdk.prove_cairo0(data).await;
-        // If authentication fails, print out the error message
-        assert!(proof.is_ok(), "Failed to prove with invalid url");
-        // If authentication fails, print out the error message for debugging purposes
+
+        assert!(proof.is_ok(), "Failed to generate proof with Cairo 0");
+
+        // Verify the generated proof if successful
         if let Err(err) = proof {
-            println!(" error: {}", err);
+            println!("Error during proof generation: {}", err);
         } else {
             let result: Result<String, ProverSdkErrors> = sdk.verify(proof.unwrap()).await;
             assert!(result.is_ok(), "Failed to verify proof");
-            assert_eq!(result.unwrap(), "true");
+            assert_eq!(result.unwrap(), "true", "Proof verification failed");
         }
+
         Ok(())
     }
 
     #[tokio::test]
     async fn test_verify_invalid_proof() {
-        let prover_url = Url::parse("http://localhost:3040").unwrap(); // Provide an invalid URL
+        let port = env::var("PORT").unwrap();
+        let prover_url = Url::parse(&format!("http://localhost:{}", port)).unwrap();
+
         let sdk = ProverSDK::new(get_signing_key(), prover_url).await.unwrap();
+
+        // Attempt to verify an invalid proof
         let result = sdk.verify("invalid_proof".to_string()).await;
-        assert!(result.is_ok(), "Failed to verify proof");
-        assert_eq!(result.unwrap(), "false");
+
+        assert!(
+            result.is_ok(),
+            "Verification unexpectedly failed for an invalid proof"
+        );
+        assert_eq!(
+            result.unwrap(),
+            "false",
+            "Invalid proof was incorrectly verified as valid"
+        );
     }
+
     #[tokio::test]
     async fn test_prover_cairo1() -> Result<(), ProverSdkErrors> {
-        let prover_url = Url::parse("http://localhost:3040").unwrap();
+        let port = env::var("PORT").unwrap();
+        let prover_url = Url::parse(&format!("http://localhost:{}", port)).unwrap();
 
-        // Act: Attempt to authenticate with the valid private key and invalid URL for authentication
         let sdk = ProverSDK::new(get_signing_key(), prover_url).await?;
 
         let data = load_cairo1(PathBuf::from(
@@ -61,69 +78,75 @@ mod tests {
         ))
         .await?;
         let proof = sdk.prove_cairo1(data).await;
-        // If authentication fails, print out the error message
-        assert!(proof.is_ok(), "Failed to prove with invalid url");
 
-        // If authentication fails, print out the error message for debugging purposes
+        assert!(proof.is_ok(), "Failed to generate proof with Cairo 1");
+
         if let Err(err) = proof {
-            println!(" error: {}", err);
+            println!("Error during proof generation: {}", err);
         } else {
             let result: Result<String, ProverSdkErrors> = sdk.verify(proof.unwrap()).await;
             assert!(result.is_ok(), "Failed to verify proof");
-            assert_eq!(result.unwrap(), "true");
+            assert_eq!(result.unwrap(), "true", "Proof verification failed");
         }
+
         Ok(())
     }
 
     #[tokio::test]
     async fn test_invalid_url_auth() -> Result<(), ProverSdkErrors> {
-        // Arrange: Set up any necessary data or dependencies
-        let prover_url = Url::parse("http://wrong:1234").unwrap(); // Provide an invalid URL for authentication
+        // Provide an invalid URL for SDK initialization
+        let prover_url = Url::parse("http://wrong:1234").unwrap();
 
-        // Act: Attempt to authenticate with the valid private key and invalid URL for authentication
         let result = ProverSDK::new(get_signing_key(), prover_url).await;
-        // Assert: Check that authentication fails due to invalid URL
+
+        // Assert that SDK initialization fails due to the invalid URL
         assert!(
             result.is_err(),
-            "Expected authentication to fail with invalid URL for authentication"
+            "Expected SDK initialization to fail with an invalid URL"
         );
-        // If authentication fails, print out the error message
+
         if let Err(err) = result {
-            println!("Error message: {}", err);
+            println!("Error during SDK initialization: {}", err);
         }
+
         Ok(())
     }
 
     #[tokio::test]
-    async fn test_invalid_prover() -> Result<(), ProverSdkErrors> {
-        // Arrange: Set up any necessary data or dependencies
-        let prover_url: Url = Url::parse("http://localhost:3040").unwrap();
+    async fn test_invalid_prover_data() -> Result<(), ProverSdkErrors> {
+        let port = env::var("PORT").unwrap();
+        let prover_url = Url::parse(&format!("http://localhost:{}", port)).unwrap();
 
-        // Act: Attempt to authenticate with the valid private key and valid URL for authentication
         let sdk = ProverSDK::new(get_signing_key(), prover_url).await?;
-        //Act: Load wrong prover input to test invalid prover version
 
+        // Load Cairo 1 prover input data (intentional mismatch)
         let data = load_cairo1(PathBuf::from(
             "../examples/Cairo/fibonacci_prover_input.json",
         ))
         .await?;
 
+        // Attempt to prove using Cairo 0 with incorrect input
         let proof = sdk.prove_cairo0(data).await;
-        // If authentication fails, print out the error message
-        assert!(proof.is_err(), "Failed to prove with invalid url");
+
+        // Assert that proof generation fails due to incorrect input
+        assert!(
+            proof.is_err(),
+            "Expected proof generation to fail with incorrect input"
+        );
+
         Ok(())
     }
 
     #[tokio::test]
     async fn test_register() -> Result<(), ProverSdkErrors> {
-        let prover_url: Url = Url::parse("http://localhost:3040").unwrap();
+        let port = env::var("PORT").unwrap();
+        let prover_url = Url::parse(&format!("http://localhost:{}", port)).unwrap();
+
         let new_key = ProverAccessKey::generate();
 
-        // Act: Attempt to authenticate with the valid private key
+        // Initialize the SDK and register the new key
         let mut sdk = ProverSDK::new(get_signing_key(), prover_url).await?;
         sdk.register(new_key.0.verifying_key()).await?;
-
-        // If authentication fails, print out the error message
 
         Ok(())
     }
--- scripts/e2e_test.sh
+++ scripts/e2e_test.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 IMAGE_NAME="http_prover_test"
-
+CONTAINER_ENGINE="${CONTAINER_ENGINE:-podman}"
+PORT="${PORT:-3040}"
 # Check if the image already exists
-if podman images | grep -q "$IMAGE_NAME"; then
+if $CONTAINER_ENGINE images | grep -q "$IMAGE_NAME"; then
     echo "Image $IMAGE_NAME already exists. Skipping build step."
 else
     echo "Image $IMAGE_NAME does not exist. Building the image..."
-    podman build -t $IMAGE_NAME .
+    $CONTAINER_ENGINE build -t $IMAGE_NAME .
     if [ $? -ne 0 ]; then
         echo "Failed to build the image. Exiting."
         exit 1
     fi
 fi
 
-podman run -d --replace --name http_prover_test \
-    -p 3040:3000 localhost/http_prover_test \
+$CONTAINER_ENGINE run -d --replace --name http_prover_test \
+    -p $PORT:3000 localhost/http_prover_test \
     --jwt-secret-key "jwt" \
     --message-expiration-time 3600 \
     --session-expiration-time 3600 \
@@ -27,4 +28,4 @@ fi
 
 cargo test --no-fail-fast --workspace --verbose -- --test-threads=1
 
-podman stop $IMAGE_NAME
+$CONTAINER_ENGINE stop $IMAGE_NAME
