50b6c18ba372641b7dfc81ba454c076e27998c6c
Mateusz Chudkowski <mateuszchudy03@gmail.com>
2024-09-02 21:25:51 +0200

    docker instead of podman

--- scripts/e2e_test.sh
+++ scripts/e2e_test.sh
@@ -3,11 +3,11 @@
 set -eux
 IMAGE_NAME="http-prover-test"
 # Check if the image already exists
-if podman images | grep -q "$IMAGE_NAME"; then
+if docker images | grep -q "$IMAGE_NAME"; then
     echo "Image $IMAGE_NAME already exists. Skipping build step."
 else
     echo "Image $IMAGE_NAME does not exist. Building the image..."
-    podman build -t $IMAGE_NAME .
+    docker build -t $IMAGE_NAME .
     if [ $? -ne 0 ]; then
         echo "Failed to build the image. Exiting."
         exit 1
@@ -24,7 +24,7 @@ KEYGEN_OUTPUT=$(cargo run -p keygen)
 ADMIN_PUBLIC_KEY=$(echo "$KEYGEN_OUTPUT" | grep "Public key" | awk '{print $3}' | tr -d ',' | tr -d '[:space:]')
 ADMIN_PRIVATE_KEY=$(echo "$KEYGEN_OUTPUT" | grep "Private key" | awk '{print $3}' | tr -d ',' | tr -d '[:space:]')
 
-podman run -d --replace --name http_prover_test \
+docker run -d --replace --name http_prover_test \
     -p 3040:3000 $IMAGE_NAME \
     --jwt-secret-key "secret" \
     --message-expiration-time 3600 \
@@ -34,4 +34,4 @@ podman run -d --replace --name http_prover_test \
 
 PRIVATE_KEY=$PRIVATE_KEY PROVER_URL="http://localhost:3040" ADMIN_PRIVATE_KEY=$ADMIN_PRIVATE_KEY cargo test --no-fail-fast --workspace --verbose
 
-podman stop http_prover_test
+docker stop http_prover_test
